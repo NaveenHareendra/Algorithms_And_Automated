00a07dc3eafa7aaf95acba2b3cccfeaa862e3cd4
Naveen Hareendra <naveen.hareendra@hotmail.com>
2025-05-11 20:51:53 +0530

    Some optimization

---
+++ Two_Characters/two-characters/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        abcdefghijklmnopqrstuvwxyzxzx
+        String s = "tlymrvjcylhqifsqtyyzfaugtibkkghfhyzxqbsizkjguqlqwwetyofqihtpkmpdlgthfybfhhmjerjdkybwppwrdapirukcshkpngayrdruanjtziknnwxmsjpnuswllymhkmztsrcwwzmlbcoakswwffveobbvzinkhnmvwqhpfednhsuzmffaebi";
+        int result = Result.alternate(s);
+//
+        System.out.println("Size of biggest string with two chars: "+result);
+    }
+}
+//seses
+
+//test case: pvmaigytciycvjdhovwiouxxylkxjjyzrcdrbmokyqvsradegswrezhtdyrsyhg
+//asdcbsdcagfsdbgdfanfghbsfdab
